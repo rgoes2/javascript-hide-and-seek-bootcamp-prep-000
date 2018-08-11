a260a39486e1055948b2ebe33d34a2fa20e035d4
Renan Góes <renangoes12@gmail.com>
2018-08-11 18:41:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+function increaseRankBy(n){
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list');
+  for (let i = 0 ; i < lis.length ; i++){
+    lis[i].innerHTML = (i + 1);
+  }
+}
+function deepestChild(){
+return document.querySelector('#grand-node div div div div')
+}
